7ee9f9b644ed08efe23073eb1807a6eb98ad03f2
Julee Kim <summerbreeze0323@gmail.com>
2021-02-17 19:28:18 +0900

    GET MOVIE Query

--- src/routes/Detail.js
+++ src/routes/Detail.js
@@ -1,1 +1,23 @@
-export default () => 'Detail';
+import { useParams } from 'react-router-dom';
+import { gql, useQuery } from '@apollo/client';
+
+const GET_MOVIE = gql`
+  query getMovie($id: Int!) {
+    movie(id: $id) {
+      title
+      medium_cover_image
+      language
+      rating
+      description_intro
+    }
+  }
+`
+
+export default () => {
+  const { id } = useParams();
+  const { loading, error, data } = useQuery(GET_MOVIE, {
+    variables: { id: +id }
+  });
+  if(loading) return 'Loading...'
+  if(data && data.movie) return data.movie.title
+}
