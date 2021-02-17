f6f107a5769711b0790f880555d2d02a624b3739
Julee Kim <summerbreeze0323@gmail.com>
2021-02-17 19:38:05 +0900

    컴포넌트 스타일 변경

--- src/routes/Detail.js
+++ src/routes/Detail.js
@@ -1,5 +1,6 @@
 import { useParams } from 'react-router-dom';
 import { gql, useQuery } from '@apollo/client';
+import styled from "styled-components";
 
 const GET_MOVIE = gql`
   query getMovie($id: Int!) {
@@ -11,13 +12,56 @@ const GET_MOVIE = gql`
       description_intro
     }
   }
-`
+`;
+
+const Container = styled.div`
+  height: 100vh;
+  background-image: linear-gradient(-45deg, #d754ab, #fd723a);
+  width: 100%;
+  display: flex;
+  justify-content: space-around;
+  align-items: center;
+  color: white;
+`;
+
+const Column = styled.div`
+  margin-left: 10px;
+`;
+
+const Title = styled.h1`
+  font-size: 65px;
+  margin-bottom: 15px;
+`;
+
+const Subtitle = styled.h4`
+  font-size: 35px;
+  margin-bottom: 10px;
+`;
+
+const Description = styled.p`
+  font-size: 28px;
+`;
+
+const Poster = styled.div`
+  width: 25%;
+  height: 60%;
+  background-color: transparent;
+`;
 
 export default () => {
   const { id } = useParams();
   const { loading, error, data } = useQuery(GET_MOVIE, {
     variables: { id: +id }
   });
-  if(loading) return 'Loading...'
-  if(data && data.movie) return data.movie.title
+  
+  return (
+    <Container>
+      <Column>
+        <Title>Name</Title>
+        <Subtitle>English · 4.5</Subtitle>
+        <Description>lorem ipsum lalalla </Description>
+      </Column>
+      <Poster></Poster>
+    </Container>
+  );
 }
