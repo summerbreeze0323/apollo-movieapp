274afcb96bae9309c81d2429b1b6fcd38ae7dc4c
Julee Kim <summerbreeze0323@gmail.com>
2021-02-17 17:25:13 +0900

    GET MOVIES Query

--- src/apollo.js
+++ src/apollo.js
@@ -1,7 +1,7 @@
 import { ApolloClient, InMemoryCache } from '@apollo/client';
 
 const client = new ApolloClient({
-  uri: 'https://movieql.now.sh/',
+  uri: 'http://localhost:4000',
   cache: new InMemoryCache()
 });
 
--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,1 +1,20 @@
-export default () => <h1>Home</h1>;
+import React from 'react';
+import { gql, useQuery } from '@apollo/client';
+
+const GET_MOVIES = gql`
+  {
+    movies {
+      id
+      medium_cover_image
+    }
+  }
+`
+
+export default () => {
+  const { loading, error, data } = useQuery(GET_MOVIES);
+  if (loading) return 'Loading...';
+
+  if (data && data.movies) return data.movies.map(movie => <h1>{movie.id}</h1>);
+  
+  return <h1>Home</h1>;
+}
