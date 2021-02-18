0960aa0b87c7b60e499ad7831ba411543efa6f35
Julee Kim <summerbreeze0323@gmail.com>
2021-02-18 15:00:34 +0900

    Local State part One

--- src/apollo.js
+++ src/apollo.js
@@ -2,7 +2,12 @@ import { ApolloClient, InMemoryCache } from '@apollo/client';
 
 const client = new ApolloClient({
   uri: 'http://localhost:4000',
-  cache: new InMemoryCache()
+  cache: new InMemoryCache(),
+  resolvers: {
+    Movie: {
+      isLiked: () => false
+    }
+  }
 });
 
 export default client;
